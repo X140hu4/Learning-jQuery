8a0f77824e5bc693ae7573cc76770a6cd0a7c513
zhaojunmeng <zhaojun.meng@gmail.com>
2012-11-12 04:47:40 +0000

    finish chapter 02

--- Chapter 2/02/02.js
+++ Chapter 2/02/02.js
@@ -5,5 +5,9 @@ $(document).ready(function() {
         $('a[href$=".pdf"]').addClass('pdflink');
         $('a[href^="http"][href*="henry"]').addClass('henrylink');
         $('tr:nth-child(odd)').addClass('alt');
-        $('td:contains(Henry)').addClass('highlight');
+        $('td:contains(Henry)').parent().children().addClass('highlight');
+
+        $('a').filter(function() {
+            return this.hostname && this.hostname != location.hostname;
+            }).addClass('external');
         });
