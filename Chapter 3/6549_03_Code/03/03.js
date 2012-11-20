9f35cddd62a22dd725d0e8d2e9c4eae336ae092a
zhaojunmeng <zhaojun.meng@gmail.com>
2012-11-20 11:14:13 +0800

    Finish chapter 03.

--- Chapter 3/6549_03_Code/03/03.js
+++ Chapter 3/6549_03_Code/03/03.js
@@ -1,24 +1,52 @@
 $(document).ready(function() {
-	$('#switcher-default').addClass('selected');
-
-	$('#switcher').click(function(event) {
-		if ($(event.target).is('button')) {
-
-			var bodyClass = event.target.id.split('-')[1];
-
-			$('body').removeClass().addClass(bodyClass);
-
-			$('#switcher button').removeClass('selected');
-			$(tevent.targe).addClass('selected');
-			event.stopPropagation();
-		} else {
-			$('#switcher button').toggleClass('hidden');
-		}
-	});
-
-	$('#switcher h3').hover(function() {
+	$('#switcher').hover(function() {
 		$(this).addClass('hover');
 	}, function() {
 		$(this).removeClass('hover');
+	});
+
+	var toggleSwitcher = function(event) {
+		if (!$(event.target).is('button')) {
+			$('#switcher button').toggleClass('hidden');
+		}
+	};
+
+	$('#switcher').bind('click', toggleSwitcher);
+
+	$('#switcher').click();
+
+	var setBodyClass = function(className) {
+		$('body').removeClass().addClass(className);
+
+			$('#switcher button').removeClass('selected');
+			$('#switcher-' + className).addClass('selected');
+
+			$('#switcher').unbind('click', toggleSwitcher);
+
+			if( className == 'default') {
+				$('#switcher').bind('click', toggleSwitcher);
+			}
+	};
+
+	$('#switcher-default').addClass('selected');
+
+	var triggers = {
+		D: 'default',
+		N: 'narrow',
+		L: 'large',
+	};
+
+	$('#switcher').click(function(event) {
+		if ($(event.target).is('button')) {
+			var bodyClass = event.target.id.split('-')[1];
+			setBodyClass(bodyClass);
+		}
+	});
+
+	$(document).keyup(function(event) {
+		var key = String.fromCharCode(event.keyCode);
+		if (key in triggers) {
+			setBodyClass(triggers[key]);
+		}
 	});
 });
