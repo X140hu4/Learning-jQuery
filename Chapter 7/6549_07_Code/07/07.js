9f40a82ed22233b5b116417fec5f210c9d25f354
zhaojunmeng <zhaojun.meng@gmail.com>
2012-12-18 23:53:34 +0800

    Update Chapter 7/7-18.

--- Chapter 7/6549_07_Code/07/07.js
+++ Chapter 7/6549_07_Code/07/07.js
@@ -3,15 +3,64 @@ $(document).ready(function() {
 		timeout: 2000,
 		speed: 200,
 		pause: true,
+		before: function() {
+			$('#slider').slider('value', $('#books li').index(this));
+		}
 	});
+
+	if ( $.cookie('cyclePaused') ) {
+		$('#books').cycle('pause');
+	}
 
 	$('<div id="books-controls"></div>').insertAfter('#books');
 	$('<button>Pause</button>').click(function() {
 		$('#books').cycle('pause');
+		$.cookie('cyclePaused', 'y');
 		return false;
+	}).button({
+		icons: {primary: 'ui-icon-pause'}
 	}).appendTo('#books-controls');
 	$('<button>Resume</button>').click(function() {
-		$('ul:paused').cycle('resume');
+		var $paused = $('ul:paused');
+		if ($paused.length) {
+			$paused.cycle('resume');
+			$.cookie('cyclePaused', null);
+		} else {
+			$(this).effect('shake', {
+				distance: 10,
+				duration: 80,
+			});
+		}
 		return false;
+	}).button({
+		icons: {primary: 'ui-icon-play'}
 	}).appendTo('#books-controls');
+	
+	$('<div id="slider"></div>').slider({
+		min: 0,
+		max: $('#books li').length - 1,
+		slide: function(event, ui) {
+			$('#books').cycle(ui.value);
+		}
+	}).appendTo('#books-controls');
+
+	$('#books').hover(function() {
+		$('#books li .title').animate({
+			backgroundColor: '#eee',
+			color: '#000',
+		}, 1000);
+	}, function() {
+		$('#books li .title').animate({
+			backgroundColor: '#000',
+			color: '#fff',
+		}, 1000);
+	});
+
+	$('h1').click(function() {
+		$(this).toggleClass('highlighted', 'slow', 'easeInExpo');
+	});
+
+	$('#books li .title').resizable({
+		handles: 's'
+	});
 });
